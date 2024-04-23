9e2ad1f058389076a6e426e2be8aa4e83ce0764c
Alex Almeida <alex.pans@gmail.com>
2024-04-23 19:19:27 -0300

    Criação de estrutura de tela e consumo de endpoint de Filmes (#3)
    * Implementação concreta do consumo de API
    - Criação de estrutura de paginação dos endpoins swapi (será descontinuado)
    - Criação de arquivo de configuração para gestão dos endpoins
    
    Signed-off-by: Alex Almeida <alex.pans@gmail.com>
    
    * Adaptar gateway para consumir tanto swapi quanto backend do desafio
    
    Signed-off-by: Alex Almeida <alex.pans@gmail.com>
    
    * Adicioção de readme e renomeação de namespace de contrato da swapi
    
    Signed-off-by: Alex Almeida <alex.pans@gmail.com>
    
    ---------
    
    Signed-off-by: Alex Almeida <alex.pans@gmail.com>

---
+++ Starls.Assets/Starls.Assets.Service/Gateway/FilmGateway.cs
@@ -0,0 +1,34 @@
+﻿using Starls.Assets.DTO;
+using Starls.Assets.DTO.Configuration;
+using Starls.Assets.Service.Gateway.Extensions;
+using Starls.Assets.Service.Gateway.Interfaces;
+
+namespace Starls.Assets.Service.Gateway
+{
+    public class FilmGateway : BaseGateway, IFilmGateway
+    {
+        private readonly ProviderConfiguration FilmProviderConfiguration;
+
+        public FilmGateway(IHttpClientFactory httpClientFactory, AppConfiguration appConfiguration) :
+            base(httpClientFactory, appConfiguration.FilmProviderConfiguration.Name)
+        {
+            this.FilmProviderConfiguration = appConfiguration.FilmProviderConfiguration;
+        }
+
+        public async Task<IEnumerable<Film>> GetFilmsAsync()
+        {
+            var urlPath = this.GetApiResourcePath(this.FilmProviderConfiguration.Resource);
+
+            if (this.FilmProviderConfiguration.Paginated)
+            {
+                var pagedResult = await this.GetPagedAsync<SwApiResponseModel.Film>(urlPath);
+
+                return pagedResult.ToDto().Results;
+            }
+
+            var result = await this.GetManyAsync<Film>(urlPath);
+
+            return result ?? [];
+        }
+    }
+}
