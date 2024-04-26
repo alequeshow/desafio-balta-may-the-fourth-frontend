f9af9b18d09fa113cd58e4ad970d076ba94efde2
Alex Almeida <alex.pans@gmail.com>
2024-04-26 10:11:11 -0300

    Correção do startup e configuração de dependencias
    Signed-off-by: Alex Almeida <alex.pans@gmail.com>

--- Starls.Assets/Starls.Assets.Presentation/Program.cs
+++ Starls.Assets/Starls.Assets.Presentation/Program.cs
@@ -15,16 +15,11 @@ var configuration = builder.Configuration.Get<AppConfiguration>() ?? new();
 
 builder.Services.AddSingleton(configuration);
 
-// Add HttpClient instances for each endpoint
+// Setup client instances for each endpoint provider individually
+
 builder.Services.AddHttpClient(configuration.FilmProviderConfiguration.Name, client =>
 {
     client.BaseAddress = new Uri(configuration.FilmProviderConfiguration.Url);
-});
-
-
-builder.Services.AddHttpClient(configuration.CharacterProviderConfiguration.Name, client =>
-{
-    client.BaseAddress = new Uri(configuration.CharacterProviderConfiguration.Url);
 });
 
 builder.Services.AddHttpClient(configuration.VehicleProviderConfiguration.Name, client =>
@@ -37,20 +32,30 @@ builder.Services.AddHttpClient(configuration.PlanetProviderConfiguration.Name, c
     client.BaseAddress = new Uri(configuration.PlanetProviderConfiguration.Url);
 });
 
-builder.Services.AddHttpClient(configuration.SharshipProviderConfiguration.Name, client => 
+builder.Services.AddHttpClient(configuration.CharacterProviderConfiguration.Name, client =>
 {
-    client.BaseAddress = new Uri(configuration.SharshipProviderConfiguration.Url);
+    client.BaseAddress = new Uri(configuration.CharacterProviderConfiguration.Url);
 });
 
+builder.Services.AddHttpClient(configuration.StarshipProviderConfiguration.Name, client =>
+{
+    client.BaseAddress = new Uri(configuration.StarshipProviderConfiguration.Url);
+});
+
+
+//Setup gateway dependencies
 
 builder.Services.AddScoped<IFilmGateway, FilmGateway>();
 
 builder.Services.AddScoped<IVehicleGateway, VehicleGateway>();
 
-builder.Services.AddScoped<ICharacterGateway, CharacterGateway>();
-
 builder.Services.AddScoped<IPlanetGateway, PlanetGateway>();
 
+builder.Services.AddScoped<ICharacterGateway, CharacterGateway>();
+
+builder.Services.AddScoped<IStarshipGateway, StarshipGateway>();
+
+//Setup services dependencies
 
 builder.Services.AddScoped<IFilmService, FilmService>();
 
@@ -60,5 +65,6 @@ builder.Services.AddScoped<IPlanetService, PlanetService>();
 
 builder.Services.AddScoped<ICharacterService, CharacterService>();
 
+builder.Services.AddScoped<IStarshipService, StarshipService>();
 
 await builder.Build().RunAsync();
